e9a6f254c652b052eee798a4023427f4260d5659
starmastar1126 <starmastar1126@gmail.com>
2024-06-30 08:02:14 -0500

    updated dotenv issue and v1 path

--- babel.config.js
+++ babel.config.js
@@ -26,12 +26,16 @@ module.exports = function (api) {
           },
         },
       ],
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-      }],
       '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
+      ['babel-plugin-dotenv-import', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }]
     ],
   };
 };
